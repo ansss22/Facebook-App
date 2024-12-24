89c0fa4eddc96c817cf423f45ed059cf2d7fe2a7
Anass El Amrany <elamranyanass22@gmail.com>
2024-12-24 23:04:48 +0100

    first commit

---
+++ Monface/Monface/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render 
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
